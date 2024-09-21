3b6052dd7fc722d3dceff79ad039b7469bd371ba
Humdiess <hmzinchannel@gmail.com>
2024-09-21 08:47:40 +0700

    SEO

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -33,7 +33,7 @@ const HeroSection = () => {
       {/* Glowing Introduction */}
       <BlurIn
         word="Hi, I'm Masyhudi"
-        className="text-7xl font-extrabold text-transparent pb-4 bg-clip-text bg-gradient-to-r from-cyan-400 via-indigo-500 to-pink-500 drop-shadow-[0_0_25px_rgba(100,255,255,0.9)] animate-pulse"
+        className="text-7xl font-extrabold text-transparent pb-4 bg-clip-text bg-gradient-to-r from-cyan-400 via-indigo-500 to-pink-500 drop-shadow-[0_0_25px_rgba(100,255,255,0.9)]"
       />
 
       <BlurFade delay={0.2} inView>
